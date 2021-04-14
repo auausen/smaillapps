0223d2f9946b76ba2e291e2df1edaf2752c8b596
naka <ryousen.naka@microworld.co.jp>
2021-04-14 17:55:50 +0900

    java

--- jpa-paging-sorting/src/main/java/com/codeforsort/jpapagingsorting/service/EmployeeService.java
+++ jpa-paging-sorting/src/main/java/com/codeforsort/jpapagingsorting/service/EmployeeService.java
@@ -1,4 +1,22 @@
 package com.codeforsort.jpapagingsorting.service;
 
+import com.codeforsort.jpapagingsorting.model.Employee;
+import com.codeforsort.jpapagingsorting.repository.EmployeeCriteriaRepository;
+import com.codeforsort.jpapagingsorting.repository.EmployeeRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeCriteriaRepository employeeCriteriaRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository,
+                           EmployeeCriteriaRepository employeeCriteriaRepository) {
+        this.employeeRepository = employeeRepository;
+        this.employeeCriteriaRepository = employeeCriteriaRepository;
+    }
+
+    public Page<Employee>
 }
